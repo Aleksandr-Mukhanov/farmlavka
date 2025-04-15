f087d7db1ba8ef66c26d57fe9964072978b89428
root <root@3213631-qm36400.twc1.net>
2025-04-15 10:47:42 +0300

    edit 15.04.2025

--- local/ajax/sendFormBlog.php
+++ local/ajax/sendFormBlog.php
@@ -10,20 +10,29 @@ if (!empty($_POST)) {
     $articleName = $_POST['articlename'];
     $articleUrl = $_POST['articleurl'];
     // file_put_contents($_SERVER['DOCUMENT_ROOT'].'/testRegEmail.txt',print_r($email,1));
-        Event::send(array(
-            "EVENT_NAME" => "READ_LATER",
-            'MESSAGE_ID' => 93,
-            "LID" => "s1",
-            "C_FIELDS" => array(
-                "USER_NAME" => $name,
-                "USER_EMAIL" => $email,
-                "ARTICLE_NAME" => $articleName,
-                "ARTICLE_URL" => $articleUrl,
-            ),
-        ));
+        // Event::send(array(
+        //     "EVENT_NAME" => "READ_LATER",
+        //     'MESSAGE_ID' => 93,
+        //     "LID" => "s1",
+        //     "C_FIELDS" => array(
+        //         "USER_NAME" => $name,
+        //         "USER_EMAIL" => $email,
+        //         "ARTICLE_NAME" => $articleName,
+        //         "ARTICLE_URL" => $articleUrl,
+        //     ),
+        // ));
+
+        $mailFields = [
+          "USER_NAME" => $name,
+          "USER_EMAIL" => $email,
+          "ARTICLE_NAME" => $articleName,
+          "ARTICLE_URL" => $articleUrl,
+        ];
+
+        CEvent::Send("READ_LATER", "s1", $mailFields);
 
     $message = 'Ссылка на статью успешно отправлена на ваш email!';
-    
+
 } else {
     $message = 'Произошла ошибка, повторите вашу заявку.';
 }
